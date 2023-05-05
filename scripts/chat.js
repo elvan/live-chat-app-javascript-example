bb1ecb399e8f867dbe26913c4e3e7184a23d3f9b
Elvan Hidayat <elvanhd@gmail.com>
2023-05-05 22:48:32 +0700

    real-time listener

--- scripts/chat.js
+++ scripts/chat.js
@@ -22,11 +22,19 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+  getChats(callback) {
+    this.chats.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === 'added') {
+          callback(change.doc.data());
+        }
+      });
+    });
+  }
 }
 
 const chatroom = new Chatroom('gaming', 'aaron');
 
-chatroom
-  .addChat('hello everyone')
-  .then(() => console.log('chat added'))
-  .catch((err) => console.log(err));
+chatroom.getChats((data) => {
+  console.log(data);
+});
